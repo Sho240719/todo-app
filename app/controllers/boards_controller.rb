7cc8a639ad740ea1d722b591b558a4fed30c4df3
Sho <sho84.kitzxo@gmail.com>
2024-08-30 14:14:50 +0900

    createアクション機能を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+
   def index
     @boards = Board.all
   end
@@ -6,4 +7,20 @@ class BoardsController < ApplicationController
   def new
     @board = current_user.boards.build
   end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to root_path, notice: "Taskを追加しました"
+    else
+      flash.now[:error] = "Taskを追加できませんでした"
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
+
 end
