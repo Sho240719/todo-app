65d9c963b1d426aa33ff51b2b4fb78c4533c4ea6
Sho <sho84.kitzxo@gmail.com>
2024-09-01 00:00:42 +0900

    update action を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -23,6 +23,16 @@ class BoardsController < ApplicationController
     @board = current_user.boards.find(params[:id])
   end
 
+  def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to root_path, notice: "更新しました"
+    else
+      flash.now[:error] = "更新できませんでした"
+      render :edit
+    end
+  end
+
   private
   def board_params
     params.require(:board).permit(:title, :content)
