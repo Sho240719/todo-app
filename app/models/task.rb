17c156fd91eaf2ad29f8c9d5ccc1570519640f77
Sho <sho84.kitzxo@gmail.com>
2024-09-06 14:25:27 +0900

    validationの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,6 +17,10 @@
 #  index_tasks_on_user_id   (user_id)
 #
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :deadline, presence: true
+
   belongs_to :user
   belongs_to :board
 end
