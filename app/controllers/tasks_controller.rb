8d51c7f8cdb43f892cc80a4cf25824f871111bf3
Sho <sho84.kitzxo@gmail.com>
2024-09-06 14:18:30 +0900

    create actionの作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,5 +5,19 @@ class TasksController < ApplicationController
   end
 
   def create
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.build(task_params)
+    @task.user = current_user
+    if @task.save
+      redirect_to board_path(@board), notice: '新たなTaskを追加しました'
+    else
+      flash.now[:error] = 'Taskを追加できませんでした'
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline)
   end
 end
