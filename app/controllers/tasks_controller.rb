199b8bb316fea0f3de064204fc254a55abf526c1
Sho <sho84.kitzxo@gmail.com>
2024-09-10 15:02:33 +0900

    tedit機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,8 @@ class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def show
-    board = Board.find(params[:board_id])
-    @task = board.tasks.find(params[:id])
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
   end
 
   def new
@@ -21,6 +21,11 @@ class TasksController < ApplicationController
       flash.now[:error] = 'Taskを追加できませんでした'
       render :new
     end
+  end
+
+  def edit
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
   end
 
   private
