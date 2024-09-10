1081aa0a5d66ef9f43779bb17094848f041245a0
Sho <sho84.kitzxo@gmail.com>
2024-09-10 13:16:02 +0900

    taskにshow アクションの機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'boards#index'
 
   resources :boards do
-    resources :tasks, only: [:new, :create]
+    resources :tasks, only: [:show, :new, :create]
   end
 end
