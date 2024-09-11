0859567522e54b8a6f51ae59f93a05b26785d33f
Sho <sho84.kitzxo@gmail.com>
2024-09-11 16:13:36 +0900

    create actionを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,22 @@ class CommentsController < ApplicationController
     @task = @board.tasks.find(params[:task_id])
     @comment = @task.comments.build(user: current_user)
   end
+
+  def create
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.find(params[:task_id])
+    @comment = @task.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to board_task_path(@board, @task), notice: 'commentを追加しました'
+    else
+      flash.now[:error] = 'commentを追加できませんでした'
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
