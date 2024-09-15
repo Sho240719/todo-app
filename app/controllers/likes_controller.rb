269b2eaa51a8ef60bd91ab6f2cd14ec52467cdc3
Sho <sho84.kitzxo@gmail.com>
2024-09-15 23:39:51 +0900

    createアクション

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,11 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
+
+  def create
+    board = Board.find(params[:board_id])
+    task = board.tasks.find(params[:task_id])
+    comment = task.comments.find(params[:comment_id])
+    comment.likes.create!(user_id: current_user.id)
+    redirect_to board_task_path(board, task)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   resources :boards do
     resources :tasks, except: [:index] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create] do
         resource :like, only: [:create]
+      end
     end
   end
 
