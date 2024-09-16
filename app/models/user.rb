7f5154b80cac238d0aba20096e65b3f39b44db81
Sho <sho84.kitzxo@gmail.com>
2024-09-16 14:42:47 +0900

    いいねした記事を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :favorite_comments, through: :likes, source: :comment
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
