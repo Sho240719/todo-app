f77dbc9b17bacb9c610f081afa21338275cf2f5d
Sho <sho84.kitzxo@gmail.com>
2024-09-16 13:25:31 +0900

    destroyアクション

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :boards do
     resources :tasks, except: [:index] do
       resources :comments, only: [:new, :create] do
-        resource :like, only: [:create]
+        resource :like, only: [:create, :destroy]
       end
     end
   end
