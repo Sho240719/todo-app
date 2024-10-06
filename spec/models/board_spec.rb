b313b769ddb170f71f8b3c7c06080e97eeaaa02e
Sho <sho84.kitzxo@gmail.com>
2024-10-07 07:00:44 +0900

    保存に失敗する場合のテストを追加

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
+  let!(:user) do
+    User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+  end
+
   context 'タイトルと内容が入力されている場合' do
-    before do
-      user = User.create!({
-        email: 'test@example.com',
-        password: 'password'
-      })
-      @board = user.boards.build({
+    let!(:board) do
+      user.boards.build({
         title: Faker::Lorem.characters(number: 10),
         content: Faker::Lorem.characters(number: 100)
       })
     end
 
     it 'ボードを保存できる' do
-      expect(@board).to be_valid
+      expect(board).to be_valid
+    end
+  end
+
+  context 'タイトルの文字が一文字の場合' do
+    let!(:board) do
+      user.boards.create({
+        title: Faker::Lorem.characters(number: 1),
+        content: Faker::Lorem.characters(number: 100)
+      })
+    end
+
+    it 'ボードを保存できない' do
+      expect(board.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
     end
   end
 end
