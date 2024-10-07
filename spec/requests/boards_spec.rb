0a49c5b47839e6f9f4419feddd6c83c04b456768
Sho <sho84.kitzxo@gmail.com>
2024-10-07 22:25:30 +0900

    boards#indexのテストを追加

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Boards', type: :request do
+  let! (:user) { create(:user) }
+  let! (:boards) { create_list(:board, 3, user: user) }
+
   describe 'GET /boards' do
     it '200ステータスが返ってくる' do
       get boards_path
