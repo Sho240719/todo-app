f3c1edf4de6c14c28a8b4b4177baa94e1b8262a1
Sho <sho84.kitzxo@gmail.com>
2024-10-07 16:35:18 +0900

    board factoryを登録

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe Board, type: :model do
   let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
-    let!(:board) do
-      user.boards.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 100)
-      })
-    end
+    let!(:board) { build(:board, user: user) }
 
     it 'ボードを保存できる' do
       expect(board).to be_valid
@@ -17,11 +12,10 @@ RSpec.describe Board, type: :model do
   end
 
   context 'タイトルの文字が一文字の場合' do
-    let!(:board) do
-      user.boards.create({
-        title: Faker::Lorem.characters(number: 1),
-        content: Faker::Lorem.characters(number: 100)
-      })
+    let!(:board) { build(:board, title: Faker::Lorem.characters(number: 1)) }
+
+    before do
+      board.save
     end
 
     it 'ボードを保存できない' do
