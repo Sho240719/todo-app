14feb79f08d32e8ac131b0bf701a51126b2a1e9f
Sho <sho84.kitzxo@gmail.com>
2024-10-07 17:05:00 +0900

    request spec のファイルを作成

---
+++ spec/requests/boards_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Boards', type: :request do
+  describe 'GET /boards' do
+    it '200ステータスが返ってくる' do
+      get boards_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
